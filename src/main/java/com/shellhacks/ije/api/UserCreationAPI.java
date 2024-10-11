3ad480ea93ae7a14137744ae4968f987063a6467
ian <ianborges.school@gmail.com>
2024-10-10 21:48:49 -0400

    Add input validation for login page, add sanitization for creation api

--- src/main/java/com/shellhacks/ije/api/UserCreationAPI.java
+++ src/main/java/com/shellhacks/ije/api/UserCreationAPI.java
@@ -2,6 +2,8 @@ package com.shellhacks.ije.api;
 
 import com.shellhacks.ije.model.User;
 import com.shellhacks.ije.service.UserService;
+import org.jsoup.Jsoup;
+import org.jsoup.safety.Safelist;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
@@ -10,18 +12,40 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
+import java.util.logging.Logger;
+
 @Controller
 public class UserCreationAPI {
     @Autowired
     private UserService userService;
     private final BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+    private final Logger logger = Logger.getLogger(UserCreationAPI.class.getName());
 
     @PostMapping({"/create", "/create/"})
     public String createUser(@RequestBody User user, Model model) {
+        // Sanitize user inputs
+        String sanitizedFirstName = Jsoup.clean(user.getFirstName(), Safelist.none());
+        String sanitizedLastName = Jsoup.clean(user.getLastName(), Safelist.none());
+        String sanitizedEmail = Jsoup.clean(user.getEmail(), Safelist.none());
+
+//        logger.info(sanitizedFirstName
+//                + sanitizedLastName + sanitizedEmail + user.getPassword());
+
+        // Set the sanitized values back to the user object
+        user.setFirstName(sanitizedFirstName);
+        user.setLastName(sanitizedLastName);
+        user.setEmail(sanitizedEmail);
+
+        // Hash the password
         user.setPassword(passwordEncoder.encode(user.getPassword()));
+
+        // Add the user to the database
         userService.addUser(user);
+
+        // Add the user to the model for the view
         model.addAttribute("user", user);
-        return "SignUpPage";
+
+        return "SignUpPage"; // Redirect to the signup page after creation
     }
 
     // Handle the GET request for the form page
